de8d9e06009796b8f6febac91fa0ae62dfb5fd7e
Matthew <gutt790@gmail.com>
2022-08-25 14:00:18 -0400

    Added fucntion to find latest replay file and commented what each line does

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import os
+import glob
 
-home_directory = os.path.expanduser('~')
-path = os.path.join(home_directory, 'AppData', 'Local', 'osu!', 'Replays')
-os.chdir(path)
+home_directory = os.path.expanduser('~') # Makes a path to C:/Users/HomeUser
+path = os.path.join(home_directory, 'AppData', 'Local', 'osu!', 'Replays') # Appends path to osu Replays folder
+os.chdir(path) 
 print(path)
+list_of_files = glob.glob(path+"/*.osr") # Makes a list of all files within path
+latest_file = max(list_of_files, key=os.path.getctime, default=0) # Looks through list_of_files and finds file with smallest
+print(latest_file)
