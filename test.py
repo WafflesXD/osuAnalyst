65173042fac5c34bd9d2223c53c2e783ca45e161
Matthew <gutt790@gmail.com>
2022-08-29 22:59:19 -0400

    Used lzma to parse through mouse and key data, not super elegant now will fix later

--- test.py
+++ test.py
@@ -1,43 +1,30 @@
-import os
-import glob
+from os import path, chdir
+from glob import glob
 from tkinter.filedialog import askdirectory as ad
-import struct
+from struct import unpack_from
+from lzma import decompress, FORMAT_AUTO
 
 
 def main():  # Python scripts instead of package
-    homeDir = os.path.expanduser("~")  # Makes a path to C:/Users/HomeUser
+    homeDir = path.expanduser("~")  # Makes a path to C:/Users/HomeUser
     try:
-        path = os.path.join(homeDir, "AppData/Local/osu!/")
-        os.chdir(path)
+        osuPath = path.join(homeDir, "AppData/Local/osu!/")
+        chdir(osuPath)
     except FileNotFoundError:  # If file not found then ask user to select
-        path = ad(title='Select osu! Folder')
-        os.chdir(path)
+        osuPath = ad(title='Select osu! Folder')
+        chdir(osuPath)
     else:
         print("Successfully entered osu! replays directory")
 
-    listOfFiles = glob.glob(path + "Replays/*.osr")  # All files with .osr
-    newestFile = max(listOfFiles, key=os.path.getctime, default=0)
+    listOfFiles = glob(osuPath + "Replays/*.osr")  # All files with .osr
+    newestFile = max(listOfFiles, key=path.getctime, default=0)
     # ^Finds file with newest metadata change
-    songName = os.path.basename(newestFile)
+    songName = path.basename(newestFile)
     userName = songName.partition(' ')[0]
     # Gets username from first part, using partition, of replay file name
 
     with open(newestFile, "rb") as replay:  # Opens replay file
         r = replay.read()
-        unpacked = struct.unpack_from("<bi", r, 0)  # Gamemode & version
-        unpacked1 = struct.unpack_from("<hhhhhhihbi", r, 75 + len(userName))
-        """
-        Byte offset # for version number @ 1 (i)
-        Byte offset # for 300's @ 85 then + 2 for next data point (h)
-            reference for ^^ @
-            https://osu.ppy.sh/wiki/en/Client/File_formats/Osr_%28file_format%29
-        Byte offset # for Total score @ 97 (i)
-        Byte offset # for Max Combo @ 101 (h)
-        Byte offset # for mods used @ 104 (i)
-            reference table for ^^ @ https://github.com/ppy/osu-api/wiki#mods
-        """
-        print(unpacked)
-        print(unpacked1)  # Read from binary, the game version for replay
         graphList = [[0, 1.0]]
         hexOffset = len(r.hex().split("7c")[0]) / 2  # offset before health
         for i in r.hex().split("7c"):
@@ -49,9 +36,21 @@ def main():  # Python scripts instead of package
                     hexOffset += len(i) / 2 + 1  # offset for each health data
                 except ValueError:
                     pass
+        hexOffset += 3  # Adds to graphList and finds hexOffset after health
         [print(i) for i in graphList]
-        hexOffset += 4  # Adds to graphList and finds hexOffset after health
         print(hexOffset)
+        gameData = unpack_from("<bi", r, 0)  # Gamemode & version
+        scoreData = unpack_from("<hhhhhhihbi", r, 75 + len(userName))
+        miscData = unpack_from("<qi", r, int(hexOffset))
+        onlineScoreID = unpack_from("<q", r, int(hexOffset) + 12 + miscData[1])
+        bruh = r[int(hexOffset) + 12: int(hexOffset) + 12 + miscData[1]]
+        bruh = decompress(bruh, format=FORMAT_AUTO)
+        bruh = bruh.decode("ascii")  # Will format later pulls mouse + key data
+        print(bruh)
+        print(gameData)  # Gamemode and version
+        print(scoreData)
+        print(miscData)  # Timestamp
+        print(onlineScoreID)  # Online score ID
 
 
 if __name__ == "__main__":
