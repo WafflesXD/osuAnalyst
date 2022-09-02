b1e7c9d83772136818e6c417e063fbde6f1634ee
Matthew <gutt790@gmail.com>
2022-09-01 21:05:25 -0400

    Made a separate module for the osu! replay file parser to clean up test.py, all functions showcased in test.py

---
+++ osuReplayParser.py
@@ -0,0 +1,222 @@
+from os import path
+from struct import unpack_from, calcsize
+from itertools import islice
+from lzma import decompress, FORMAT_AUTO
+from datetime import datetime, timedelta
+
+
+class Replay:
+
+    gmDict = {0: "osu!",
+              1: "osu!Taiko",
+              2: "osu!Catch",
+              3: "osu!Mania"}
+    Dict100s = {"osu!": "100s in standard: ",
+                "osu!Taiko": "150s in Taiko: ",
+                "osu!Catch": "100s in CTB: ",
+                "osu!Mania": "100s in mania: "}
+    Dict50s = {"osu!": "50s in standard: ",
+               "osu!Catch": "small fruit in CTB: ",
+               "osu!Mania": "50s in mania: "}
+    DictGekis = {"osu!": "Gekis in standard: ",
+                 "osu!Mania": "Max 300s in mania: "}
+    DictKatus = {"osu!": "Katus in standard: ",
+                 "osu!Mania": "200s in mania: "}
+    fcDict = {1: "Yes!",
+              0: "No"}
+    modsDict = {0: "No mods",
+                1: "NoFail",
+                2: "Easy",
+                4: "TouchDevice",
+                8: "Hidden",
+                16: "HardRock",
+                32: "SuddenDeath",
+                64: "DoubleTime",
+                128: "Relax",
+                256: "HalfTime",
+                512: "Nightcore",
+                1024: "Flashlight",
+                2048: "Autoplay",
+                4096: "SpunOut",
+                8192: "Relax2",
+                16384: "Perfect",
+                32768: "Key4",
+                65536: "Key5",
+                131072: "Key6",
+                262144: "Key7",
+                524288: "Key8",
+                1048576: "FadeIn",
+                2097152: "Random",
+                4194304: "Cinema",
+                8388608: "Target",
+                16777216: "Key9",
+                33554432: "KeyCoop",
+                67108864: "Key1",
+                134217728: "Key3",
+                268435456: "Key2",
+                536870912: "ScoreV2",
+                1073741824: "Mirror"}
+    hexOffset = None
+    replayLength = None
+
+    def __init__(self, replayDir):
+        self.replayDir = replayDir
+        self.replay = self.openReplayFile()
+        self.songName = path.basename(replayDir)
+        self.userName = self.getUserName()
+        self.gameMode = self.getGameMode()
+        self.gameVersion = "Release: " + self.getGameVersion()
+        self.scoreData = self.scoreData()
+        self.lifeBarData = self.getLifeBarData()
+
+        dataSize = len(self.replay.hex()) / 2
+        posDataCheck = (self.hexOffset - 3) == dataSize
+        healthDataCheck = (self.hexOffset - int(self.hexOffset)) == 0
+        if not healthDataCheck or posDataCheck:  # Check for health data
+            print("changed")
+            self.hexOffset = 77 + len(self.userName) + calcsize("<hhhhhhihbi")
+
+        self.replayDate = "Played on: " + str(self.getTime())
+        self.onlineScoreID = "Score ID: " + str(self.getOnlineScoreID())
+
+        if not healthDataCheck or not posDataCheck:
+            self.poskeyData = self.getPosKeyData()
+        else:
+            self.poskeyData = "No position or key data avaliable"
+
+    def openReplayFile(self):
+        with open(self.replayDir, "rb") as r:  # Opens replay file
+            return r.read()
+
+    def getUserName(self):
+        userNameHex = (str(self.replay.hex()).split("0b")[2])[2:]
+        return bytes.fromhex(userNameHex).decode()
+
+    def extractReplayData(self):
+        gameData = unpack_from("<bi", self.replay, 0)
+        offset = 75 + len(self.userName)
+        scoreData = unpack_from("<hhhhhhihbi", self.replay, offset)
+        return gameData + scoreData
+
+    def printLifeBarData(self):
+        pLifeBarData = []
+        if self.getLifeBarData():
+            for i in self.getLifeBarData():
+                pLifeBarData.append(["Health: {}".format(i[0]),
+                                     "Time: {} sec".format(i[1] / 1000)])
+        else:
+            print("No Health Data Avaliable")
+        [print(i) for i in pLifeBarData]
+        return None
+
+    def scoreData(self):
+        scoreData = "".join(["Number of 300s: {}\nNumber of {}{}\n",
+                             "Number of {}{}\nNumber of {}{}\n",
+                             "Number of {}{}\nNumber of misses: {}\n",
+                             "Total score: {}\nGreatest combo: {}\n",
+                             "Full Combo: {}\nMods Used: {}"])
+        return scoreData.format(str(self.getNum300s()),
+                                self.Dict100s.get(self.gameMode),
+                                str(self.getNum100s()),
+                                self.Dict50s.get(self.gameMode),
+                                str(self.getNum50s()),
+                                self.DictGekis.get(self.gameMode),
+                                str(self.getNumGekis()),
+                                self.DictKatus.get(self.gameMode),
+                                str(self.getNumKatus()),
+                                str(self.getNumMisses()),
+                                str(self.getTotalScore()),
+                                str(self.getGreatestCombo()),
+                                self.getFC(),
+                                self.getMods())
+
+    def getGameMode(self):
+        return self.gmDict.get(self.extractReplayData()[0])
+
+    def getGameVersion(self):
+        return str(self.extractReplayData()[1])
+
+    def getNum300s(self):
+        return self.extractReplayData()[2]
+
+    def getNum100s(self):
+        return self.extractReplayData()[3]
+
+    def getNum50s(self):
+        return self.extractReplayData()[4]
+
+    def getNumGekis(self):
+        return self.extractReplayData()[5]
+
+    def getNumKatus(self):
+        return self.extractReplayData()[6]
+
+    def getNumMisses(self):
+        return self.extractReplayData()[7]
+
+    def getTotalScore(self):
+        return self.extractReplayData()[8]
+
+    def getGreatestCombo(self):
+        return self.extractReplayData()[9]
+
+    def getFC(self):
+        return self.fcDict.get(self.extractReplayData()[10])
+
+    def getMods(self):
+        return self.modsDict.get(self.extractReplayData()[11])
+
+    def getLifeBarData(self):
+        graphList = [[1.0, 0]]
+        self.hexOffset = len(self.replay.hex().split("7c3")[0]) / 2
+        for i in self.replay.hex().split("7c"):
+            if "2c" or "302e3" in i:
+                try:
+                    h = i.partition("2c")[0]
+                    t = i.partition("2c")[2]
+                    health = bytes.fromhex(h).decode("utf-8")
+                    time = bytes.fromhex(t).decode("utf-8")
+                    graphList.append([float(health), int(time)])
+                    self.hexOffset += len(i) / 2 + 1
+                except ValueError:
+                    pass
+            else:
+                pass
+        self.hexOffset += 3
+        return graphList
+
+    def getTime(self):
+        miscData = unpack_from("<qi", self.replay, int(self.hexOffset))
+        self.replayLength = miscData[1]
+        time = datetime(1, 1, 1) + timedelta(microseconds=miscData[0] / 10)
+        time.strftime("%Y-%m-%d %H:%M:%S")
+        return time
+
+    def getOnlineScoreID(self):
+        self.hexOffset += calcsize("<qi")
+        hexOffset2 = int(self.hexOffset) + self.replayLength
+        onlineScoreID = unpack_from("<q", self.replay, hexOffset2)
+        if onlineScoreID[0] == 0:
+            return "Score not submitted"
+        else:
+            return onlineScoreID[0]
+
+    def getPosKeyData(self):
+        print(((self.hexOffset - 3) == len(self.replay.hex()) / 2))
+        hexOffset2 = self.hexOffset + self.replayLength
+        print(int(self.hexOffset))
+        print(int(hexOffset2))
+        print()
+        posData = self.replay[int(self.hexOffset): int(hexOffset2)]
+        posData = decompress(posData, format=FORMAT_AUTO)
+        posData = posData.decode("ascii")  # Pulls mouse + key data
+        posDataList = []
+        posDataRange = len(posData.split(",")) - 1
+        for i in islice(posData.split(","), 0, posDataRange):
+            timeFromLastEvent = int(i.split("|")[0])
+            xPos = float(i.split("|")[1])
+            yPos = float(i.split("|")[2])
+            keyCombo = int(i.split("|")[3])
+            posDataList.append([timeFromLastEvent, xPos, yPos, keyCombo])
+        # [print(i) for i in posDataList]  # Organized posData into list
+        return posDataList
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from os import path, chdir
 from glob import glob
 from tkinter.filedialog import askdirectory as ad
-from struct import unpack_from, calcsize
-from itertools import islice
-from lzma import decompress, FORMAT_AUTO
+from osuReplayParser import Replay
 
 
 def main():  # Python scripts instead of package
+
     homeDir = path.expanduser("~")  # Makes a path to C:/Users/HomeUser
     try:
         osuPath = path.join(homeDir, "AppData/Local/osu!/")
@@ -19,65 +18,18 @@ def main():  # Python scripts instead of package
 
     listOfFiles = glob(osuPath + "Replays/*.osr")  # All files with .osr
     newestFile = max(listOfFiles, key=path.getctime, default=0)
-    # ^Finds file with newest metadata change
-    songName = path.basename(newestFile)
-    userName = songName.partition(" ")[0]
-    # Gets username from first part, using partition, of replay file name
 
-    with open(newestFile, "rb") as replay:  # Opens replay file
-        r = replay.read()
-
-        graphList = [[1.0, 0]]
-        hexOffset = len(r.hex().split("7c3")[0]) / 2  # offset before health
-        # Handles other "|" before health data
-        for i in r.hex().split("7c"):
-            if "2c" or "302e3" in i:  # Check if "1," or "0.,integer"
-                try:
-                    h = bytes.fromhex(i.partition("2c")[0]).decode("utf-8")
-                    t = bytes.fromhex(i.partition("2c")[2]).decode("utf-8")
-                    graphList.append([float(h), int(t)])
-                    hexOffset += len(i) / 2 + 1  # offset for each health data
-                except ValueError:
-                    pass
-        hexOffset += 3  # Adds to graphList and finds hexOffset after health
-        # [print(i) for i in graphList]
-        print(hexOffset)
-
-        gameData = unpack_from("<bi", r, 0)  # Gamemode & version
-        print(gameData)  # Gamemode and version
-
-        scoreData = unpack_from("<hhhhhhihbi", r, 75 + len(userName))
-        print(scoreData)  # 300 100 50 Gekis Katus Misses Score Combo FC Mods
-
-        healthDataCheck = (hexOffset - int(hexOffset)) == 0
-        # Handles no life bar data
-        healthDataCheck1 = (hexOffset - 3) == len(r.hex()) / 2
-        # Handles no life bar or posData
-        if not healthDataCheck or healthDataCheck1:  # Check for health data
-            hexOffset = 77 + len(userName) + calcsize("<hhhhhhihbi")
-
-        miscData = unpack_from("<qi", r, int(hexOffset))
-        print(miscData)  # Timestamp
-
-        hexOffset += calcsize("<qi")
-        onlineScoreID = unpack_from("<q", r, int(hexOffset) + miscData[1])
-        print(onlineScoreID)  # Online score ID
-        # Returns 0 if not completed or not played online
-
-        if not healthDataCheck or not healthDataCheck1:
-            posData = r[int(hexOffset): int(hexOffset) + miscData[1]]
-            posData = decompress(posData, format=FORMAT_AUTO)
-            posData = posData.decode("ascii")  # Pulls mouse + key data
-            posDataList = [[None, None, None, None]]
-            posDataRange = len(posData.split(",")) - 1
-            for i in islice(posData.split(","), 0, posDataRange):
-                timeFromLastEvent = int(i.split("|")[0])
-                xPos = float(i.split("|")[1])
-                yPos = float(i.split("|")[2])
-                keyCombo = int(i.split("|")[3])
-                posDataList.append([timeFromLastEvent, xPos, yPos, keyCombo])
-            # [print(i) for i in posDataList]  # Organized posData into list
-            # print(mouseData)
+    r = Replay(newestFile)
+    print(r.replay)
+    print(r.songName)
+    print(r.userName)
+    print(r.gameMode)
+    print(r.gameVersion)
+    print(r.scoreData)
+    r.printLifeBarData()
+    print(r.poskeyData)
+    print(r.replayDate)
+    print(r.onlineScoreID)
 
 
 if __name__ == "__main__":
